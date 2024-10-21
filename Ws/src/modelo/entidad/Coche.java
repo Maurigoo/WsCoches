95f8037cd7b26b71f140c3a8f17abf478630053e
Jose Mauricio Herbas Becerra <jose.mauricio.herbas@bootcamp-upgrade.com>
2024-10-21 20:42:21 +0200

    creada la entidad Coche

--- Ws/src/modelo/entidad/Coche.java
+++ Ws/src/modelo/entidad/Coche.java
@@ -1,5 +1,42 @@
 package modelo.entidad;
 
+import java.util.Objects;
+
 public class Coche {
 
+	int id;
+	String marca;
+	String modelo;
+	TIPO_MOTOR motor;
+	
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public TIPO_MOTOR getMotor() {
+		return motor;
+	}
+	public void setMotor(TIPO_MOTOR motor) {
+		this.motor = motor;
+	}
+	
+	
+	@Override
+	public String toString() {
+		
+		return "Id : " + id + ", marca : " + marca + "  modelo : " + modelo + " y con motor" + motor;
+	}	
 }
---
+++ Ws/src/modelo/entidad/TIPO_MOTOR.java
@@ -0,0 +1,6 @@
+package modelo.entidad;
+
+public enum TIPO_MOTOR {
+
+		GASOLINA, DIESEL, HIDROGENO;
+}
