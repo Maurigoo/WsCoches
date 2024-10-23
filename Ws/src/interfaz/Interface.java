d6bd7e85249dec5afd11ab7e4e251323af27a75f
Maurigoo <mauysergio@gmail.com>
2024-10-23 15:36:02 +0200

    1

--- Ws/src/interfaz/Interface.java
+++ Ws/src/interfaz/Interface.java
@@ -1,5 +1,21 @@
 package interfaz;
 
+
+import java.util.Scanner;
+import modelo.entidad.Coche;
+import modelo.negocio.GestorCoche;
+
 public class Interface {
+	
+	    private GestorCoche gc = null;
+	    private Scanner scString = new Scanner(System.in); // Para leer texto
+	    private Scanner scInt = new Scanner(System.in);    // Para leer números
+	    
+	    public void mostrarInterface() {
+	    	System.out.println("Bienvenido/a a nuestra app de coches");
+	    	gc = new GestorCoche();
+	    	int respuesta = 0;
+	    	
+	    }
 
 }
