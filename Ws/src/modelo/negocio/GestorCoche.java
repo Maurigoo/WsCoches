fc8efbb2e8c1098ca46ebd49c6f6d955c4eb983f
Avril2504 <avrilpino@gmail.com>
2024-10-23 19:34:18 +0200

    metodos gestor

--- Ws/src/modelo/negocio/GestorCoche.java
+++ Ws/src/modelo/negocio/GestorCoche.java
@@ -1,5 +1,38 @@
 package modelo.negocio;
 
 public class GestorCoche {
+	
+	/**
+	 * Metodo que verifica si la marca o modelo pasado por parametro
+	 * esta vacio o es null
+	 * @param marca marca a evaluar
+	 * @param modelo modelo a evaluar
+	 * @return <b>1<b> si la marca esta vacia, solo con espacios en
+	 * blanco o null <b>2<b> si el modelo esta vacio, solo con espacios
+	 * 
+	 */
+	public int estaVacio(String marca, String modelo) {
+		if(marca == null || marca.isBlank()) {
+			return 1;	
+		}
+		if(modelo == null || modelo.isBlank()) {
+			return 2;
+		}
+		return 3;
+	}
+	
+	/**
+	 * Metodo que no permite que el id sea menor que 0
+	 * @param id id a evaluar
+	 * @return false en caso que sea menor que 0, true en
+	 * caso contrario
+	 */
+	public boolean esNegativo(int id) {
+		if(id < 0) {
+			return false;	
+		}
+		return true;
+		
+	}
 
 }
